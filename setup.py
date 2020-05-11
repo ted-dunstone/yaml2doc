87f69362c227a40f5acae62add6d43d4335f7c0b
Ted Dunstone <ted@biometix.com>
2020-05-11 10:12:58 +0800

    next fix

--- setup.py
+++ setup.py
@@ -1,87 +1,46 @@
-"""A setuptools based setup module.
+# -*- coding: utf-8 -*-
 
-"""
+from setuptools import setup
 
-from setuptools import setup, find_packages
-from os import path
-from io import open
+from yaml2rst import __version__
 
-here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
-
+long_description = "\n\n".join([
+    open("README.md").read(),
+    ])
 
 setup(
-    name='yaml2doc',  # Required
-
-    version='0.5',  # Required
-
-    description='Document YAML',  # Optional
-
-    long_description=long_description,  # Optional
-    long_description_content_type='text/markdown',  # Optional (see note above)
-    url='https://github.com/pypa/sampleproject',  # Optional
-
-    author='Biometix',  # Optional
-
-    # Classifiers help users find your project by categorizing it.
-    #
-    classifiers=[  # Optional
-        'Development Status :: 5',
-        # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-        # Pick your license as you wish
-        'License :: Biometix',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-    ],
-
-    keywords='yaml md markdown documentation pandoc',  # Optional
-
-    # When your source code is in a subdirectory under the project root, e.g.
-    # `src/`, it is necessary to specify the `package_dir` argument.
-    #package_dir={'': 'px_build_doc'},  # Optional
-
-    # You can just specify package directories manually here if your project is
-    # simple. Or you can use find_packages().
-    #
-    #py_modules=["export_doc"],
-    #
-    packages=find_packages(where='yaml2doc'),  # Required
-
-    python_requires='>=3.5, <4',
-
-    # This field lists other packages that your project depends on to run.
-    # Any package you put here will be installed by pip when your project is
-    # installed, so they must be valid existing projects.
-    #
-    # For an analysis of "install_requires" vs pip's requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
-    #`install_requires=['Pillow'],  # Optional
-
-
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.
-    #
-    # If using Python 2.6 or earlier, then these have to be included in
-    # MANIFEST.in as well.
-    include_package_data=True, 
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # `pip` to create the appropriate form of executable for the target
-    # platform.
-    #
-    entry_points={  # Optional
+    name="yaml2doc",
+    version=__version__,
+    description="""A Tool for Documenting YAML Files to md and others""",
+    long_description=long_description,
+    author="Ted Dunstone & Hartmut Goebel",
+    author_email='ted@biometix.com',
+    license="GPLv3+",
+    packages=['yaml2rst'],
+    url="https://github.com/ted-dunstone/yaml2doc/yaml2rst",
+    # These are for reference only, pip is not able to download packages
+    # from github because the archives do not include the project-name.
+    download_url="https://github.com/ted-dunstone/yaml2doc/releases",
+    bugtrack_url="https://github.com/ted-dunstone/yaml2doc/issues",
+    keywords=['YML', 'YAML',
+              'rst', 'reStructuresText',
+              'literate programming'],
+    entry_points={
         'console_scripts': [
-            'yaml2doc=yaml2doc.yaml2doc:run',
-        ],
+            'yaml2doc = yaml2doc.yaml2doc:run',
+        ]
     },
-
-    project_urls={  # Optional
-    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        ('License :: OSI Approved :: '
+         'GNU General Public License v3 or later (GPLv3+)'),
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Documentation',
+    ],
 )
--- yaml2doc/__init__.py
+++ yaml2doc/__init__.py
@@ -1,1 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+yaml2doc – A  Tool for Documenting YAML Files to md and beyond
+"""
+#
+# Copyright 2015-2019 by Hartmut Goebel <h.goebel@crazy-compilers.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+
+
 import yaml2doc
+
+__author__ = "Ted Dunstone <ted@biometix.com>"
+__copyright__ = ("Copyright 2015-2019 by Hartmut Goebel "
+                 "<h.goebel@crazy-compilers.com>")
+__licence__ = "GNU General Public License version 3 or later (GPLv3+)"
+__version__ = '0.4.dev0'
