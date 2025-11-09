e8f5627bb855f18616ba11a9d41014175c8219b1
Ted Dunstone <ted@biometix.com>
2025-11-09 18:32:10 +1100

    Fix header - from old code

--- yaml2doc/yaml2doc.py
+++ yaml2doc/yaml2doc.py
@@ -3,24 +3,6 @@
 """
 yaml2doc – A  Tool for Documenting YML Files
 """
-#
-# Copyright 2015-2019 by Hartmut Goebel <h.goebel@crazy-compilers.com>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-# General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
-#
-# SPDX-License-Identifier: GPL-3.0-or-later
-#
 
 import argparse
 import sys
